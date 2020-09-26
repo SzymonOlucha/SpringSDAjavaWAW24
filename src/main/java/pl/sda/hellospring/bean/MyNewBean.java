d4a0fcb411d3f5797996816fcdb568be8aa032e8
Szymek582 <szymon.olucha@gmail.com>
2020-09-26 11:19:00 +0200

    [change] New Bean - adding toString

--- src/main/java/pl/sda/hellospring/bean/MyNewBean.java
+++ src/main/java/pl/sda/hellospring/bean/MyNewBean.java
@@ -5,6 +5,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyNewBean {
 
+
+    @Override
+    public String toString() {
+        return "MyNewBean{}";
+    }
+
     public MyNewBean() {
 
     }
